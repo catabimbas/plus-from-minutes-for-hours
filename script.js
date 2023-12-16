2bf40993f61d3177721ca3c31f33ade8d5b0cb2e
catabimbas <endermenzagm@gmail.com>
2023-12-16 15:34:59 -0300

    Agora tá bonito

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function calcMin(){
     numb1 = document.querySelector("input.minutes").value
     numb1 = Number(numb1)
     cont = cont+numb1
+    p.innerHTML = `${hour} Hora(s) ${cont} Minuto(s)`
     if (cont>=60){
         cont = numb1-60
         hour+=1
