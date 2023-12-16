6f5eb969cb20771ca54cf1b2e69753a42e91898f
catabimbas <endermenzagm@gmail.com>
2023-12-16 15:04:24 -0300

    inicio e possivel fim

---
+++ script.js
@@ -0,0 +1,14 @@
+let numb1 = document.querySelector("input.minutes")
+let cont = 0
+let hour = 0
+const p = document.querySelector("p.convert")
+function calcMin(){
+    numb1 = document.querySelector("input.minutes").value
+    numb1 = Number(numb1)
+    cont = cont+numb1
+    if (cont>=60){
+        cont = numb1-60
+        hour+=1
+        p.innerHTML = `${hour} Hora(s) ${cont} Minuto(s)`
+    }
+}
