5a52bbffa35ac7b221b75d084b0ff32e547b9fbc
catabimbas <endermenzaogamer@gmail.com>
2023-12-24 12:17:12 -0300

    Correção lógica

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ function calcMin(){
     numb1 = document.querySelector("input.minutes").value
     numb1 = Number(numb1)
     cont = cont+numb1
-    p.innerHTML = `${hour} Hora(s) ${cont} Minuto(s)`
+    console.log(numb1)
+    console.log(cont)
     if (cont>=60){
-        cont = numb1-60
+        cont= cont - 60
         hour+=1
+        console.log(cont)
+        console.log(numb1)
+        p.innerHTML = `${hour} Hora(s) ${cont} Minuto(s)`
+        cont=0
+    } else{
         p.innerHTML = `${hour} Hora(s) ${cont} Minuto(s)`
     }
 }
