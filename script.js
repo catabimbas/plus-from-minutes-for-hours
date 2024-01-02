903e64fdd8d69dd0e760699e2ba8daa062571562
catabimbas <endermenzaogamer@gmail.com>
2024-01-02 16:47:14 -0300

    Primeira parte feita

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let numb1 = document.querySelector("input.minutes")
+let geso = document.querySelector("input.minutes")
 let cont = 0
 const p = document.querySelector("p.convert")
+
+geso.addEventListener('keyup', function(e){
+    var key = e.which || e.keycode
+    if (key == 13) {
+        calcMin()
+    }
+})
 function calcMin(){
     numb1 = document.querySelector("input.minutes").value
     numb1 = Number(numb1)
     cont = cont+numb1
     p.innerHTML = `${Math.floor(cont/60)} Hora(s) ${cont%60} Minuto(s)`
+    geso.value = ''
 }
